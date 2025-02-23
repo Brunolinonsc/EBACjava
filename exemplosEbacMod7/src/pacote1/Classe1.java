bebf2ebe049de3fbc4c22ca2893c50b76b9ab9ff
BRUNO LINO NASCIMENTO <163471585+Brunolinonsc@users.noreply.github.com>
2025-02-23 10:53:33 -0300

    Aula 3: Entendendo modificadores de acesso

---
+++ exemplosEbacMod7/src/pacote1/Classe1.java
@@ -0,0 +1,13 @@
+package pacote1;
+
+public class Classe1 {
+
+    private String propiedadePrivada;
+
+    public String propiedadePublica;
+
+    String propiedadeDefault;
+
+    protected String propiedadeProtected;
+
+}
---
+++ exemplosEbacMod7/src/pacote1/Classe2.java
@@ -0,0 +1,16 @@
+package pacote1;
+
+public class Classe2 {
+
+    private String propiedadePrivada;
+
+    public String propiedadePublica;
+
+    String propiedadeDefault;
+
+    protected String propiedadeProtected;
+
+    public static void main(String[] args) {
+        Classe1 classe1 = new Classe1();
+    }
+}
