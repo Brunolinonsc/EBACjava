79b28d2e43d61f7ebd7facfe93d3381796458933
BRUNO LINO NASCIMENTO <163471585+Brunolinonsc@users.noreply.github.com>
2025-02-23 12:00:16 -0300

    ok

--- exemplosEbacMod7/src/pacote2/Classe3.java
+++ exemplosEbacMod7/src/pacote2/Classe3.java
@@ -6,6 +6,6 @@ import pacote1.Classe2;
 public class Classe3 extends Classe2 {
     
     public static void main(String[] args) {
-        Classe2 classe2 = new Classe2();
+        //Classe2 classe2 = new Classe2();
     }
 }
